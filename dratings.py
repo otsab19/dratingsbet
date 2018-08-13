eacc50015448b9b1c947cadd704f9cc8f997bd61
otsab <chaliseutsab@gmail.com>
2018-08-13 21:39:04 +0545

    Loop

--- dratings.py
+++ dratings.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import pudb
 import os
 import re
 import sys
@@ -32,22 +33,55 @@ class DratingsBet():
             res = requests.get(links)
             html_sel = html.fromstring(res.content)
             # particular tr
-            data = html_sel.xpath('//table[1]//tr[positon()>1]')
-            td=[]
+            data = html_sel.xpath('//table[1]//tr[position()>1]')
+            td = []
             for i in range(len(data)//3):
                 t = []
                 t = data[i*3:i*3+3:1]
                 td.append(t)
-            for node in data:
-                # convert to string
-                val_string = etree.tostring(node)
-                # to element
-                val_ele = html.fromstring(val_string)
-                # extract list of text from td
-                value = val_ele.xpath('//tr/td//text()')
-                print(value)
+            for node in td:
+                li = {}
+                for i,nod in enumerate(node):
+                    pudb.set_trace()
+                    val_string = etree.tostring(nod)
+                    val_ele = html.fromstring(val_string)
+                    value = val_ele.xpath('//tr/td//text()')
+                    print(value)
+                    try:
+                        if i==0:
+                            li['Date'] = value[0]
+                            li['HomeTeam'] = value[2]
+                            li['AwayTeam'] = value[1]
+                            li['HomeTeamper'] = value[4]
+                            li['HomeTeamML'] = value[5]
+                            li['HomeTeamProj'] = value[6]
+                            li['TotalGoals'] = value[7]
+                        elif i==1:
+                            li['AwayTeamPer'] = value[1]
+                            li['AwayTeamML'] = value[2]
+                            li['AwayteamProj'] = value[3]
+                        else:
+                            pudb.set_trace()
+                            li['DrawPer'] = value[1]
+                            li['DawML'] = value[2]
+                            li['DrawProj'] = value[3]
+                    except:
+                        pass
+                try:   
+                    self.parse(li) 
+                except:
+                    pass
+            # for node in data:
+            #    # convert to string
+            #    val_string = etree.tostring(node)
+            #    # to element
+            #    val_ele = html.fromstring(val_string)
+            #    # extract list of text from td
+            #    value = val_ele.xpath('//tr/td//text()')
+            #    print(value)
 
-    # def parse():
+    def parse(self, li):
+        print(li)
 
 
 if __name__ == "__main__":
