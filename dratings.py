b480f1293fd9297222238abeb1858ca3c492ec4d
otsab <chaliseutsab@gmail.com>
2018-08-14 18:05:42 +0545

    Add file write and yattag indent

--- dratings.py
+++ dratings.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import pudb
 import os
 import re
@@ -15,58 +15,72 @@ import requests
 from bs4 import BeautifulSoup
 from dateutil.rrule import rrule, DAILY
 import errno
-#from yattag import indent
+from yattag import indent
 from lxml import etree
+
+# define some constants
+HEADER_FOOTBALL = ['Date','Away','Home','Odds to Win','ML Prediction','Score Proj','Total Goals']
+PATH = ''
+
 class DratingsBet():
 
     def __init__(self):
         self.links = []
-
+        self.leagues = []
     def scrape_links(self):
         res = requests.get('https://www.dratings.com/')
         html_sel = html.fromstring(res.content)
         self.links = html_sel.xpath('//table[2]//tr/td/center/a/@href')
+        self.leagues = html_sel.xpath('//table[2]//tr/td/center/a/text()')
         print(self.links)
 
     def start_requests(self):
-        for links in self.links:
+        for links,league in zip(self.links,self.leagues):
             res = requests.get(links)
             html_sel = html.fromstring(res.content)
-            # particular tr
-            data = html_sel.xpath('//table[1]//tr[position()>1]')
-            td = []
-            for i in range(len(data)//3):
-                t = []
-                t = data[i*3:i*3+3:1]
-                td.append(t)
-            for node in td:
-                li = {}
-                for i,nod in enumerate(node):
-                    pudb.set_trace()
-                    val_string = etree.tostring(nod)
-                    val_ele = html.fromstring(val_string)
-                    value = val_ele.xpath('//tr/td//text()')
-                    print(value)
-                    try:
-                        if i==0:
-                            li['Date'] = value[0]
-                            li['HomeTeam'] = value[2]
-                            li['AwayTeam'] = value[1]
-                            li['HomeTeamper'] = value[4]
-                            li['HomeTeamML'] = value[5]
-                            li['HomeTeamProj'] = value[6]
-                            li['TotalGoals'] = value[7]
-                        elif i==1:
-                            li['AwayTeamPer'] = value[1]
-                            li['AwayTeamML'] = value[2]
-                            li['AwayteamProj'] = value[3]
-                        else:
-                            pudb.set_trace()
-                            li['DrawPer'] = value[1]
-                            li['DawML'] = value[2]
-                            li['DrawProj'] = value[3]
-                    except:
-                        pass
+            # check tables for prediction table
+            table = html_sel.xpath('//table')
+            tab_tmp = list(table)
+            for i, tab in enumerate(table):
+                headers = html_sel.xpath('//table['+str(i+1)+']//tr//th/text()')
+                if headers != HEADER_FOOTBALL:
+                    tab_tmp.remove(tab)
+            table = tab_tmp
+            for index in range(len(table)):
+                # particular tr
+                data = html_sel.xpath('//table['+str(index+1)+']//tr[position()>1]')
+                td = []
+                for i in range(len(data)//3):
+                    t = []
+                    t = data[i*3:i*3+3:1]
+                    td.append(t)
+                for node in td:
+                    li = {}
+                    for i, nod in enumerate(node):
+                        val_string = etree.tostring(nod)
+                        val_ele = html.fromstring(val_string)
+                        value = val_ele.xpath('//tr/td//text()')
+                        print(value)
+                        try:
+                            li['sport'] = "Soccer"
+                            li['League'] = league 
+                            if i == 0:
+                                li['Date'] = value[0]
+                                li['Hometeam'] = value[2]
+                                li['Awayteam'] = value[1]
+                                li['Pred1'] = value[4]
+                                #li['HomeTeamML'] = value[5]
+                                li['Predtotalpointshome'] = value[6]
+                                li['Predtotalpoints'] = value[7]
+                            elif i==1:
+                                li['Pred2'] = value[1]
+                                #li['AwayTeamML'] = value[2]
+                                li['Predtotalpointsaway'] = value[3]
+                            else:
+                                li['PredX'] = value[1]
+                                #li['DawML'] = value[2]
+                        except:
+                            pass
                 try:   
                     self.parse(li) 
                 except:
@@ -81,7 +95,39 @@ class DratingsBet():
             #    print(value)
 
     def parse(self, li):
-        print(li)
+        pudb.set_trace()
+        root = Element('Matches')
+        Match = SubElement(root, 'Match')
+        Source = SubElement(Match, 'Source')
+        Source.text = "Dratings"
+        Sport = SubElement(Match, 'Sport')
+        # Date = SubElement(Match, 'Date')
+        Sport.text = li['sport']
+        for elem in li:
+            xml_attr = SubElement(Match, elem)
+            xml_attr.text = li[elem]
+        filename = li['Hometeam']+'-'+li['Awayteam']
+        try:
+            PATH = os.path.join(os.getcwd(), li['sport'])
+            os.makedirs(PATH)
+        except OSError as exc: 
+            if exc.errno == errno.EEXIST and os.path.isdir(PATH):
+                pass
+            else:
+                raise
+        try:
+            PATH = os.path.join(os.getcwd(), li['sport'],li['league'])
+            os.makedirs(PATH)
+        except OSError as exc: 
+            if exc.errno == errno.EEXIST and os.path.isdir(PATH):
+                pass
+            else:
+                raise
+        # convert to lxml element to string    
+        root_string = tostring(root)
+        with open(os.path.join(PATH, filename), 'w', encoding="utf-8") as fl:
+            print(filename)
+            fl.write(indent(root_string.decode('utf-8')))
 
 
 if __name__ == "__main__":
